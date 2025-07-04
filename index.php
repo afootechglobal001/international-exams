83ba3d95211a7729968db14559053e5a7188d8ce
Mike Afolabi <afootechglobal@gmail.com>
2025-07-04 18:17:36 +0100

    update 07 @ 03-07-2025 on landing page cursor text

--- index.php
+++ index.php
@@ -62,53 +62,56 @@ Best place to register ACT exam in Nigeria, where to register international exam
         </div>
 
         <script type="text/javascript">
-            // List of sentences
-            var _CONTENT = ["INTERNATIONAL EXAMINATION CENTERS IN NIGERIA", "15% OFF INTERNATIONAL EXAMS REGISTRATION FEE", "LEARNING MADE EASY WITH OUR PHYSICAL AND ONLINE CLASSES", "GET ADMISSION INTO UNIVERSITY AND COLLEGE ABROAD"];
-            // Current sentence being processed
-            var _PART = 0;
-            // Character number of the current sentence being processed 
-            var _PART_INDEX = 0;
-            // Element that holds the text
-            var _ELEMENT = document.querySelector("#pageTitle");
-            // Implements typing effect
-            function Type() {
-                var text = _CONTENT[_PART].substring(0, _PART_INDEX + 1);
-                _ELEMENT.innerHTML = text;
-                _PART_INDEX++;
+        // List of sentences
+        var _CONTENT = ["INTERNATIONAL EXAMINATION CENTERS IN NIGERIA", "15% OFF INTERNATIONAL EXAMS REGISTRATION FEE",
+            "LEARNING MADE EASY WITH OUR PHYSICAL AND ONLINE CLASSES",
+            "GET ADMISSION INTO UNIVERSITY AND COLLEGE ABROAD"
+        ];
+        // Current sentence being processed
+        var _PART = 0;
+        // Character number of the current sentence being processed 
+        var _PART_INDEX = 0;
+        // Element that holds the text
+        var _ELEMENT = document.querySelector("#pageTitle");
+        // Implements typing effect
+        function Type() {
+            var text = _CONTENT[_PART].substring(0, _PART_INDEX + 1);
+            _ELEMENT.innerHTML = text;
+            _PART_INDEX++;
 
-                // If full sentence has been displayed then start to delete the sentence after some time
-                if (text === _CONTENT[_PART]) {
-                    clearInterval(_INTERVAL_VAL);
-                    setTimeout(function() {
-                        _INTERVAL_VAL = setInterval(Delete, 2);
-                    }, 5000);
-                }
+            // If full sentence has been displayed then start to delete the sentence after some time
+            if (text === _CONTENT[_PART]) {
+                clearInterval(_INTERVAL_VAL);
+                setTimeout(function() {
+                    _INTERVAL_VAL = setInterval(Delete, 2);
+                }, 5000);
             }
-            // Implements deleting effect
-            function Delete() {
-                var text = _CONTENT[_PART].substring(0, _PART_INDEX - 1);
-                _ELEMENT.innerHTML = text;
-                _PART_INDEX--;
+        }
+        // Implements deleting effect
+        function Delete() {
+            var text = _CONTENT[_PART].substring(0, _PART_INDEX - 1);
+            _ELEMENT.innerHTML = text;
+            _PART_INDEX--;
 
-                // If sentence has been deleted then start to display the next sentence
-                if (text === '') {
-                    clearInterval(_INTERVAL_VAL);
+            // If sentence has been deleted then start to display the next sentence
+            if (text === '') {
+                clearInterval(_INTERVAL_VAL);
 
-                    // If last sentence then display the first one, else move to the next
-                    if (_PART == (_CONTENT.length - 1))
-                        _PART = 0;
-                    else
-                        _PART++;
-                    _PART_INDEX = 0;
+                // If last sentence then display the first one, else move to the next
+                if (_PART == (_CONTENT.length - 1))
+                    _PART = 0;
+                else
+                    _PART++;
+                _PART_INDEX = 0;
 
-                    // Start to display the next sentence after some time
-                    setTimeout(function() {
-                        _INTERVAL_VAL = setInterval(Type, 50);
-                    }, 200);
-                }
+                // Start to display the next sentence after some time
+                setTimeout(function() {
+                    _INTERVAL_VAL = setInterval(Type, 50);
+                }, 200);
             }
-            // Start the typing effect on load
-            _INTERVAL_VAL = setInterval(Type, 50);
+        }
+        // Start the typing effect on load
+        _INTERVAL_VAL = setInterval(Type, 50);
         </script>
     </div>
 

0949320ac3b00f9d3761fc4c9cd20d28d7f58fa5
Hon Paul Emmanuel <seunemmanuel107@gmail.com>
2025-07-12 16:07:28 +0100

    update 3 @ 12-07-2025 on dashboaed design to emma dev

--- admin/dashboard/meta.php
+++ admin/dashboard/meta.php
@@ -8,13 +8,17 @@
 <link href="<?php echo $websiteUrl?>/style/icons-1.11.3/font/bootstrap-icons.css" type="text/css" rel="stylesheet">
 <link href="<?php echo $websiteUrl?>/style/animate.css" type="text/css" rel="stylesheet" media="all">
 <link href="<?php echo $websiteUrl?>/style/aos.css" type="text/css" rel="stylesheet" />
-<link href="<?php echo $websiteUrl?>/style/paramount.css?v=<?php echo $codeVersion?>" type="text/css"
-    rel="stylesheet" />
-<link href="<?php echo $websiteUrl?>/admin/dashboard/style/main-style.css?v=<?php echo $codeVersion?>" type="text/css"
-    rel="stylesheet" />
+<link href="<?php echo $websiteUrl?>/style/paramount.css?v=<?php echo $codeVersion?>" type="text/css"rel="stylesheet" />
+<link href="<?php echo $websiteUrl?>/admin/dashboard/style/main-style.css?v=<?php echo $codeVersion?>" type="text/css"rel="stylesheet" />
+<link href="<?php echo $websiteUrl?>/admin/dashboard/style/nav-style.css?v=<?php echo $codeVersion?>" type="text/css"rel="stylesheet" />
+<link href="<?php echo $websiteUrl?>/admin/dashboard/style/jquery.datetimepicker.css" type="text/css"rel="stylesheet"/>
 
 <script src="<?php echo $websiteUrl?>/js/jquery-v3.6.1.min.js"></script>
+<script src="<?php echo $websiteUrl?>/js/paramount.js?v=<?php echo $codeVersion?>"></script>
 <script src="<?php echo $websiteUrl?>/js/textfield-selectfield.js?v=<?php echo $codeVersion?>"></script>
 <script src="<?php echo $websiteUrl?>/js/aos.js"></script>
 
+<script src="<?php echo $websiteUrl?>/admin/dashboard/js/jquery.datetimepicker.js"></script>
+<script src="<?php echo $websiteUrl?>/admin/dashboard/js/chart.min.js"></script>
+<script src="<?php echo $websiteUrl?>/admin/dashboard/js/canvasjs.min.js" type="text/javascript"></script>
 <script src="<?php echo $websiteUrl?>/admin/dashboard/js/scripts.js?v=<?php echo $codeVersion?>"></script>

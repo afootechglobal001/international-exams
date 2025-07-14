4f7fbf34e5c1e5a6df3ab4495ba905940b830951
candyvado <candyboi25@gmail.com>
2025-07-14 16:53:08 +0100

    updated 14-07-2025 on signup form

---
+++ portal/config/function.php
@@ -0,0 +1,10 @@
+<?php
+class allClass
+{
+        function _pagesImage() { ?>
+            <div class="left-graphic-div" title="International Exams"></div>      
+        <?php }
+  
+} //end of class
+$callclass = new allClass();
+?>

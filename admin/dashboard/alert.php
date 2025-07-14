62fe5130805645fb6f891daa2e273265f099cf91
Hon Paul Emmanuel <seunemmanuel107@gmail.com>
2025-07-14 12:22:32 +0100

    update 1 @ 14-07-2025 on index and dashboard design

--- admin/dashboard/alert.php
+++ admin/dashboard/alert.php
@@ -5,22 +5,21 @@
     </div>
 </div>
 
-<div id="get-more-div">
+<div id="get-form-more-div">
     <div class="alert-loading-div">
-        <div class="icon"><img src="<?php echo $website_url?>/all-images/images/loading.gif" width="20px" alt="Loading"/></div> 
+        <div class="icon"><img src="<?php echo $websiteUrl?>/images/loading.gif" width="20px" alt="Loading"/></div> 
         <div class="text"><p>LOADING...</p></div>
     </div>
 </div>
 
 <div id="get-more-div-secondary">
     <div class="alert-loading-div">
-        <div class="icon"><img src="<?php echo $website_url?>/all-images/images/loading.gif" width="20px" alt="Loading"/></div> 
+        <div class="icon"><img src="<?php echo $websiteUrl?>/images/loading.gif" width="20px" alt="Loading"/></div> 
         <div class="text"><p>LOADING...</p></div>
     </div>
 </div>
 
 <div class="sidenavdiv-in" onclick="_close_side_nav()"></div>
-</div>
 
 
 

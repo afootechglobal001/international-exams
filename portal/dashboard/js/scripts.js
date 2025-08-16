67d8d816aca81d89a3a69477e98cc8b393ad7610
Mike Afolabi <afolabi.o@eleadernetwork.com>
2025-08-16 21:39:50 +0100

    update student dashboard v5

--- portal/dashboard/js/scripts.js
+++ portal/dashboard/js/scripts.js
@@ -1,16 +1,12 @@
 function _getActivePage(props) {
-	const {
-        page = '',
-        divid = '',
-		pageContainer='page-content'
-    } = props;
-    _getActivePageLink(divid);
-	if(page){
-		_getPage({page: page, pageContainer: pageContainer,  url: portalLocalUrl});
-	}
+  const { page = "", divid = "", pageContainer = "page-content" } = props;
+  _getActivePageLink(divid);
+  if (page) {
+    _getPage({ page: page, pageContainer: pageContainer, url: portalLocalUrl });
+  }
 }
 
-function _getActivePageLink(divid){
-	$('#dashboard, #exam, #transactionHistory, #subscriptionHistory, #settings').removeClass('active');
-	$("#"+divid).addClass('active');
+function _getActivePageLink(divid) {
+  $("#dashboard, #ebook, #exam, #transactions").removeClass("active");
+  $("#" + divid).addClass("active");
 }
--- portal/dashboard/side-bar.php
+++ portal/dashboard/side-bar.php
@@ -1,20 +1,22 @@
 <!-- Sidebar -->
 <div class="sidebar-div">
     <div class="sidebar-div-in">
-
         <nav>
             <ul>
                 <li class="active" title="Dashboard" id="dashboard"
-                    onclick="_getActivePage({page:'dashboard', divid:'dashboard'});"><i class="bi bi-speedometer2"></i>
-                    Dashboard</li>
+                    onclick="_getActivePage({page:'dashboard', divid:'dashboard'});">
+                    <i class="bi bi-speedometer2"></i> Dashboard
                 </li>
-                <li title="Exam" id="exam" onclick="_getActivePage({page:'exam', divid:'exam'});"><i
-                        class="bi bi-filetype-pdf"></i> Download E-books</li>
-                <li title="Exam" id="exam" onclick="_getActivePage({page:'exam', divid:'exam'});"><i
-                        class="bi bi-journal-text"></i> My Exams</li>
-                <li title="Transactions History" id="transactionHistory"
-                    onclick="_getActivePage({page:'transactionHistory', divid:'transactionHistory'});"><i
-                        class="bi bi-credit-card-2-back"></i> Transactions</li>
+                <li title="E-Books" id="ebook" onclick="_getActivePage({page:'ebook', divid:'ebook'});">
+                    <i class="bi bi-filetype-pdf"></i> Download E-books
+                </li>
+                <li title="My Exams" id="exam" onclick="_getActivePage({page:'exam', divid:'exam'});">
+                    <i class="bi bi-journal-text"></i> My Registered Exams
+                </li>
+                <li title="Transactions History" id="transactions"
+                    onclick="_getActivePage({page:'transactions', divid:'transactions'});">
+                    <i class="bi bi-credit-card-2-back"></i> Transactions
+                </li>
             </ul>
         </nav>
 
@@ -23,7 +25,6 @@
                 <li title="Settings"><i class="bi bi-gear"></i> Settings</li>
                 <li title="Log-Out"><i class="bi bi-power"></i> Log-Out</li>
             </ul>
+        </nav>
     </div>
-
-    </nav>
 </div>

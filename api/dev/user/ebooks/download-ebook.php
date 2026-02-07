a76b2b6e5a67943fa632c47a551a963557a736da
Mike Afolabi <afolabi.o@eleadernetwork.com>
2026-02-07 21:30:46 +0100

    updates on ebook download

--- api/dev/user/ebooks/download-ebook.php
+++ api/dev/user/ebooks/download-ebook.php
@@ -17,124 +17,53 @@
 
 <?php
     //////////////////declaration of variables//////////////////////////////////////
-	$examId=trim(($data['examId']));
-    $ebookId=trim(($data['ebookId']));
-    
-
+	$examId=trim($_GET['examId']);
+    $ebookId=trim($_GET['ebookId']);
     //////////////////check for empty fields//////////////////////////////////////
-        validateEmptyField($examId, 'EXAM ID');
-        validateEmptyField($ebookId, 'EBOOK ID');
-    ///////////////////////geting sequence//////////////////////////
-        $countId='ER';
-        $sequence=$callclass->_getSequenceCount($conn, $countId);
-        $array = json_decode($sequence, true);
-        $no= $array[0]['no'];
-        $examRegistrationId=$countId.$no.date("Ymdhis");
-        
-        ///get country paymentKey
-        $query=mysqli_query($conn,"SELECT paymentKey FROM COUNTRY_TAB WHERE countryId='$countryId'") or die (mysqli_error($conn));
-        $fetchQuery=mysqli_fetch_array($query);
-        $paymentKey=$fetchQuery['paymentKey'];
+    validateEmptyField($examId, 'EXAM ID');
+    validateEmptyField($ebookId, 'EBOOK ID');
+
+    ////// get ebook details for this $examId and $ebookId
+    $select = "SELECT material, sellingPrice FROM EXAM_EBOOK_TAB WHERE examId='$examId' AND ebookId='$ebookId'";
+    $query=mysqli_query($conn,$select)or die (mysqli_error($conn));
+    $fetchQuery=mysqli_fetch_assoc($query);
+    $material=$fetchQuery['material'];
+    $sellingPrice=$fetchQuery['sellingPrice'];
 
 
-        mysqli_query($conn,"INSERT INTO `STUDENT_EXAMS_REGISTRATION_TAB`
-        (`examRegistrationId`, `countryId`, `studentId`, `examId`, `locationId`, `centreId`, `examDate`, `altExamDate`, `firstName`, `middleName`, `lastName`, `dob`, `emailAddress`, `phoneNumber`, `residentialAddress`, `genderId`, `statusId`, `createdTime`) VALUES
-        ('$examRegistrationId', '$countryId', '$studentId', '$examId', '$locationId', '$centreId', '$examDate', '$altExamDate', '$firstName', '$middleName', '$lastName', '$dob', '$emailAddress', '$phoneNumber', '$residentialAddress', '$genderId', 3, NOW())")or die (mysqli_error($conn));
+    $checkRegistrationQuery=mysqli_query($conn,"SELECT * FROM STUDENT_EXAMS_REGISTRATION_TAB WHERE studentId='$loginUserId' AND examId='$examId' AND statusId=1 LIMIT 1")or die (mysqli_error($conn));
+    $checkRegistrationCount=mysqli_num_rows($checkRegistrationQuery);
+    $isDownloadable=$checkRegistrationCount > 0 ? true : false; // Add registration status to the response
 
-        foreach ($schoolsOfInterestSegment as $schoolsOfInterest) {
-			$nameOfInstitution = $schoolsOfInterest['nameOfInstitution'];
-			$institutionCode = $schoolsOfInterest['institutionCode'];
-			$institutionLocation = $schoolsOfInterest['institutionLocation'];
-			$programId = $schoolsOfInterest['programId'];
-			$courseOfStudy = $schoolsOfInterest['courseOfStudy'];
-			/// Insert Into payment_breakdown_tab
-			mysqli_query($conn,"INSERT INTO `STUDENT_SCHOOL_OF_INTEREST_TAB` 
-            (`examRegistrationId`, `nameOfInstitution`, `institutionCode`, `institutionLocation`, `programId`, `courseOfStudy`) VALUES 
-            ('$examRegistrationId', '$nameOfInstitution', '$institutionCode', '$institutionLocation', '$programId', '$courseOfStudy')")or die (mysqli_error($conn));
-		}
-        ///// get exam fee for payment processing
-        $select = "SELECT * FROM BRANCH_EXAM_PRICING_TAB WHERE countryId='$countryId' AND examId='$examId'";
+    
+    if($isDownloadable){///start if 1
+       $response = [
+            'response' => 200,
+            'success' => true,
+            'isDownloadable'=>$isDownloadable,
+            'message' => "Proceed to download this ebook.",
+            'material' => $material
+        ];
+    }else{
+        //// get ebook details
+        $select = "SELECT ebookTitle, sellingPrice, regPix FROM EXAM_EBOOK_TAB WHERE examId='$examId' AND ebookId='$ebookId'";
         $query=mysqli_query($conn,$select)or die (mysqli_error($conn));
-        $fetchQuery=mysqli_fetch_array($query);
-        $amount=$fetchQuery['amount'];
-        $currency=$fetchQuery['currency'];
-        $examAbbr=$fetchQuery['examAbbr'];
-
-        $countId='TRANS';
-        $sequence=$callclass->_getSequenceCount($conn, $countId);
-        $array = json_decode($sequence, true);
-        $no= $array[0]['no'];
-        $transactionId=$countId.$no.date("Ymdhis");
-        
-        $transactionTypeId='PMT'; /// PAYMENT
-        $balanceBefore=$loginUserWalletBalance;
-        $balanceAfter=$balanceBefore; /// since payment is pending
-
-        if ($paymentMethodId=='CC') {
-        ///get country paymentKey
-        $query=mysqli_query($conn,"SELECT paymentKey FROM COUNTRY_TAB WHERE countryId='$countryId'") or die (mysqli_error($conn));
-        $fetchQuery=mysqli_fetch_array($query);
-        $paymentKey=$fetchQuery['paymentKey'];
-        $statusId=3; /// PENDING
-
-        mysqli_query($conn,"INSERT INTO `TRANSACTION_TAB`
-        (`countryId`, `transactionId`, `userId`, `transactionTypeId`, `paymentMethodId`, `emailAddress`, `currency`, `balanceBefore`, `amount`, `balanceAfter`, `paymentKey`, `statusId`, `createdTime`) VALUES
-        ('$countryId', '$transactionId', '$studentId', '$transactionTypeId', '$paymentMethodId', '$emailAddress', '$currency', '$balanceBefore', '$amount', '$balanceAfter', '$paymentKey', '$statusId', NOW())")or die (mysqli_error($conn));
-            $alertDetail = "User with ID $studentId and Name $loginUserFullname attempt to register for $examAbbr exam with Registration ID $examRegistrationId. Transaction ID $transactionId generated for payment of $currency $amount.";
-
-        }elseif($paymentMethodId=='BT'){
-            $paymentKey='BANK TRANSFER';
-            $statusId=3; /// PENDING
-            mysqli_query($conn,"INSERT INTO `TRANSACTION_TAB`
-            (`countryId`, `transactionId`, `userId`, `transactionTypeId`, `paymentMethodId`, `emailAddress`, `currency`, `balanceBefore`, `amount`, `balanceAfter`, `paymentKey`, `statusId`, `createdTime`) VALUES
-            ('$countryId', '$transactionId', '$studentId', '$transactionTypeId', '$paymentMethodId', '$emailAddress', '$currency', $balanceBefore, '$amount', '$balanceAfter', '$paymentKey', '$statusId', NOW())")or die (mysqli_error($conn));
-            $alertDetail = "User with ID $studentId and Name $loginUserFullname attempt to register for $examAbbr exam with Registration ID $examRegistrationId. Transaction ID $transactionId generated for payment of $currency $amount.";
-        }elseif($paymentMethodId=='WLT'){
-            if ($loginUserWalletBalance < $amount) {
-                $response = [
-                    'response' => 103,
-                    'success' => false,
-                    'message' => "INSUFFICIENT WALLET BALANCE! You have $currency $loginUserWalletBalance in your wallet. Please fund your wallet and try again.",
-                ];
-                goto end;
-            }
-            $paymentKey='WALLET';
-            $statusId=1; /// APPROVED
-            $balanceAfter=$balanceBefore - $amount; /// since payment is from wallet
-            mysqli_query($conn,"INSERT INTO `TRANSACTION_TAB`
-            (`countryId`, `transactionId`, `userId`, `transactionTypeId`, `paymentMethodId`, `emailAddress`, `currency`, `balanceBefore`, `amount`, `balanceAfter`, `paymentKey`, `statusId`, `createdTime`) VALUES
-            ('$countryId', '$transactionId', '$studentId', '$transactionTypeId', '$paymentMethodId', '$emailAddress', '$currency', '$balanceBefore', '$amount', '$balanceAfter', '$paymentKey', '$statusId', NOW())")or die (mysqli_error($conn));
-            /// update user wallet balance
-            mysqli_query($conn,"UPDATE USERS_TAB SET walletBalance='$balanceAfter' WHERE userId='$studentId'")or die (mysqli_error($conn));
-        $alertDetail = "User with ID $studentId and Name $loginUserFullname successfully paid $currency $amount from wallet for $examAbbr exam with Registration ID $examRegistrationId. Transaction ID $transactionId generated for payment.";
-
-        }else{
-            $response = [
-                'response' => 104,
-                'success' => false,
-                'message' => "INVALID PAYMENT METHOD! Please select a valid payment method and try again.",
-            ];
-            goto end;
-        }
+        $fetchQuery=mysqli_fetch_assoc($query);
         
         $response = [
             'response' => 200,
             'success' => true,
-            'message' => "EXAM REGISTRATION LOG SUCCESSFUL.",
+            'isDownloadable'=>$isDownloadable,
+            'message' => "Proceed to payment to download this ebook.",
             'data' => [
-                'examRegistrationId' => $examRegistrationId,
-                'transactionId' => $transactionId,
-                'fullName'      => $firstName." ".$middleName." ".$lastName,
-                'emailAddress'  => $emailAddress,
-                'phoneNumber'   => $phoneNumber,
-                'amount'        => $amount,
-                'currency'      => $currency,
-                'paymentKey'    => $paymentKey,
-                'paymentMethodId' => $paymentMethodId
+                'examId' => $examId,
+                'ebookId' => $ebookId,
+                'ebookData' => $fetchQuery,
+                'userFullName' => $loginUserFullname
             ]
         ];
-        $callclass->_alertSequenceAndUpdate($conn,$loginUserCountryId,$loginUserId,$loginUserFullname,$alertDetail,$ipAddress,$systemName);
-end:
+    }
 //////////////////////////////////////////////////////////////////////////////////////////////
+end:
 echo json_encode($response);
 ?>
--- api/dev/user/ebooks/fetch-ebook.php
+++ api/dev/user/ebooks/fetch-ebook.php
@@ -18,7 +18,9 @@
 <?php
 /// $loginUserId
     //////////////////declaration of variables//////////////////////////////////////
-    $select = "SELECT DISTINCT examId, examAbbr FROM EXAM_EBOOK_TAB ORDER BY examAbbr ASC";
+    // $select = "SELECT DISTINCT examId, ebookId, examAbbr, ebookTitle, sellingPrice, regpix, ebookSize, ebookPages FROM EXAM_EBOOK_TAB ORDER BY examAbbr ASC";
+    
+    $select = "SELECT DISTINCT (examId) AS examId FROM EXAM_EBOOK_TAB ORDER BY examAbbr ASC";
     $query=mysqli_query($conn,$select)or die (mysqli_error($conn));
     $allRecordCount=mysqli_num_rows($query);
     if($allRecordCount==0){///start if 1
@@ -32,29 +34,32 @@
         'response' => 200,
         'success' => true,
         'message' => "E-BOOK FETCH SUCCESSFULLY!",
-        'allRecordCount' => $allRecordCount,
         'data' => array() // Initialize the data array
     ];
 
     while ($fetchQuery = mysqli_fetch_assoc($query)) {
         $examId=$fetchQuery['examId'];
-
+        ///// for $examId
+        $getExamQuery=mysqli_query($conn,"SELECT publishId AS examId, examLogo, regTitle AS examName, examAbbr FROM PUBLISH_TAB WHERE publishId='$examId'")or die (mysqli_error($conn));
+        $getExamFetch=mysqli_fetch_assoc($getExamQuery);
+        $fetchQuery['examData']=$getExamFetch;
+       
         /////////////////// Fetch ebook per exam /////////
         $ebookData=array();
         $getEbookQuery = mysqli_query($conn, "SELECT
-        a.*,
-        b.statusName,
-        c.examLogo
-        FROM EXAM_EBOOK_TAB a
-        JOIN SETUP_STATUS_TAB b ON a.statusId=b.statusId
-        JOIN PUBLISH_TAB c ON a.examId=c.publishId
-        WHERE a.examId='$examId'") or die(mysqli_error($conn));
+        ebookId, ebookTitle, sellingPrice, regpix, ebookSize, ebookPages
+        FROM EXAM_EBOOK_TAB WHERE examId='$examId' AND statusId=1 AND sellingPrice > 0") or die(mysqli_error($conn));
         
         while ($getEbookFetch = mysqli_fetch_assoc($getEbookQuery)) {
             $ebookData[] = $getEbookFetch;
         }
         $fetchQuery['ebookData']= $ebookData;
         
+         ////confirm if this user has registered for this exam
+        $checkRegistrationQuery=mysqli_query($conn,"SELECT * FROM STUDENT_EXAMS_REGISTRATION_TAB WHERE studentId='$loginUserId' AND examId='$examId' AND statusId=1 LIMIT 1")or die (mysqli_error($conn));
+        $checkRegistrationCount=mysqli_num_rows($checkRegistrationQuery);
+        $fetchQuery['isDownloadable']=$checkRegistrationCount > 0 ? true : false; // Add registration status to the response
+
         $response['data'][] = $fetchQuery;
     }
 end:

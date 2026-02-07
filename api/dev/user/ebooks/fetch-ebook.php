06752944432a8e20ba98090f11413b6cef673743
Mike Afolabi <afolabi.o@eleadernetwork.com>
2026-02-07 13:43:23 +0100

    updates on ebook download

--- api/dev/user/ebooks/fetch-ebook.php
+++ api/dev/user/ebooks/fetch-ebook.php
@@ -16,9 +16,9 @@
 ?>
 
 <?php
+/// $loginUserId
     //////////////////declaration of variables//////////////////////////////////////
     $select = "SELECT DISTINCT examId, examAbbr FROM EXAM_EBOOK_TAB ORDER BY examAbbr ASC";
-
     $query=mysqli_query($conn,$select)or die (mysqli_error($conn));
     $allRecordCount=mysqli_num_rows($query);
     if($allRecordCount==0){///start if 1
@@ -31,7 +31,7 @@
     $response=[
         'response' => 200,
         'success' => true,
-        'message' => "E-BOOK FETCH SUCCESFFULY!",
+        'message' => "E-BOOK FETCH SUCCESSFULLY!",
         'allRecordCount' => $allRecordCount,
         'data' => array() // Initialize the data array
     ];
@@ -46,15 +46,15 @@
         b.statusName,
         c.examLogo
         FROM EXAM_EBOOK_TAB a
-        JOIN SETUP_STATUS_TAB b
-        ON a.statusId=b.statusId
-        JOIN PUBLISH_TAB c
-        ON a.examId=c.publishId
+        JOIN SETUP_STATUS_TAB b ON a.statusId=b.statusId
+        JOIN PUBLISH_TAB c ON a.examId=c.publishId
         WHERE a.examId='$examId'") or die(mysqli_error($conn));
+        
         while ($getEbookFetch = mysqli_fetch_assoc($getEbookQuery)) {
             $ebookData[] = $getEbookFetch;
         }
         $fetchQuery['ebookData']= $ebookData;
+        
         $response['data'][] = $fetchQuery;
     }
 end:

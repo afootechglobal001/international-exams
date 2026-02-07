7d1d0bdab186fc3f2579f0e20d93a00a14c146bd
Emmytech12 <seunemmanuel107@gmail.com>
2026-02-07 22:59:01 +0100

    update: update 3 @07-02-2026 -- Proceed to donwload ebooks

--- portal/dashboard/js/useEbook.js
+++ portal/dashboard/js/useEbook.js
@@ -37,12 +37,14 @@ function _initFetchEbookData(data) {
             const btnContainer = isFree
                 ? `
                     <button class="btn" title="Download"
-                        onclick="">
+                        id="downloadBtn_${ebook.ebookId}"
+                        onclick="_downloadEbook('${exam?.examData?.examId}', '${ebook.ebookId}')">
                         <i class="bi bi-cloud-download"></i> Download Now!
                     </button>
                   `
                 : `
-                    <button class="btn" title="Pay To Download">
+                    <button class="btn" title="Pay To Download"
+                        onclick="_proceedDownloadEbook('${exam?.examData?.examId}', '${ebook.ebookId}');">
                         <i class="bi bi-credit-card"></i> Pay Now!
                     </button>
                   `;
@@ -100,4 +102,99 @@ function _initFetchEbookData(data) {
     }).join("");
 
     $('#pageContent').html(content);
+}
+
+function _filtersEbooks(value) {
+  $("#pageContent > .main-content-div, .book-div").each(function () {
+    var text = $(this).text();
+    text.toLowerCase().indexOf(value.toLowerCase()) > -1
+      ? $(this).show()
+      : $(this).hide();
+  });
+}
+
+function _downloadEbook(examId, ebookId) {
+    ///// get btn text/////
+	const btnText = $(`#downloadBtn_${ebookId}`).html();
+	_btnDisable(`downloadBtn_${ebookId}`, btnText, true);
+    try {
+        _callFetchEndPoints({
+            url: `user/ebooks/download-ebook?examId=${examId}&ebookId=${ebookId}`,
+            accessKey: true,
+        })
+            .then((response) => {
+                _userValidationCheck(response.response);
+                if (response.success) {
+                    const material = response.material;
+                    _finishDownloadEbook(material);
+                    _btnDisable(`downloadBtn_${ebookId}`, btnText, false);
+                } else {
+                    _showCustomConfirm({
+                        title: "Download E-Book Error",
+                        message: response.message,
+                        alertType: "warning",
+                        trueActionBtnText: "OK",
+                    });
+                }
+            })
+            .catch((error) => {
+                console.error("Error:", error);
+                _callAjaxError(() => _downloadEbook(examId, ebookId)); // retry if needed
+                _btnDisable(`downloadBtn_${ebookId}`, btnText, false);
+            });
+    } catch (error) {
+        console.error("Error:", error);
+        _callAjaxError(() => _downloadEbook(examId, ebookId)); // retry if needed
+         _btnDisable(`downloadBtn_${ebookId}`, btnText, false);
+    }
+}
+
+function _finishDownloadEbook(material) {
+  const url = `${ebookMaterialPath}/${material}`;
+
+  // open new tab
+  const tab = window.open(url, '_blank');
+
+  // force download after tab opens
+  setTimeout(() => {
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = material;
+    a.click();
+  }, 500);
+}
+
+function _proceedDownloadEbook(examId, ebookId) {
+    $("#get-form-more-div").css({'display': 'flex','justify-content': 'center','align-items': 'center'}) .fadeIn(500);
+	try {
+		//// call endpoint //////
+		_callFetchEndPoints({
+			url: `user/ebooks/download-ebook?examId=${examId}&ebookId=${ebookId}`,
+			accessKey: true,
+		})
+		.then((response) => {
+            _userValidationCheck(response.response);
+			if (response.success && response.data) {
+    			sessionStorage.setItem("useProceedEbookDownloadSession", JSON.stringify(response.data));
+                _getForm({page: 'proceedEbookForm', url: portalOperationMiddlewareUrl});
+			} else {
+                _alertClose();
+				_showCustomConfirm({
+					title: "ERROR",
+					message: response.message,
+					alertType: "warning",
+					trueActionBtnText: "OK",
+				});
+			} 
+		 })
+		.catch((error) => {
+            _alertClose();
+			console.error("Error:", error);
+			_callAjaxError(() => _proceedDownloadEbook(examId, ebookId)); // retry if needed
+		});
+	} catch (error) {
+        _alertClose();
+		console.error("Error:", error);
+		_callCatchError(() => _proceedDownloadEbook(examId, ebookId));
+  	}
 }

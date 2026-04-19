129361553547d2728cebb5984b6a35a543020910
Emmytech12 <seunemmanuel107@gmail.com>
2026-04-19 21:28:39 +0100

    update: update 1 @19-04-2026 -- update slide pictures

--- slide.php
+++ slide.php
@@ -7,12 +7,13 @@
             <li><img src="slide-property/data/images/2.jpg" alt="2" title=""/></li>
             <li><img src="slide-property/data/images/3.jpg" alt="3" title=""/></li>
             <li><img src="slide-property/data/images/4.jpg" alt="4" title=""/></li>
+            <li><img src="slide-property/data/images/5.jpg" alt="5" title=""/></li>
+            <li><img src="slide-property/data/images/6.jpg" alt="6" title=""/></li>
+            <li><img src="slide-property/data/images/7.jpg" alt="7" title=""/></li>
         </ul>
     </div>
 
 </div>
 
 <script type="text/javascript" src="slide-property/engine/wowslider.js"></script> 
-<script type="text/javascript" src="slide-property/engine/script.js"></script> 
-
-
+<script type="text/javascript" src="slide-property/engine/script.js"></script>
